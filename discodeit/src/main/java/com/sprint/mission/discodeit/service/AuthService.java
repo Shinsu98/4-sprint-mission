1c822f07ffb556379c0dcfa2476e3de8b38103f2
ShinEunSu <94344629+Shinsu98@users.noreply.github.com>
2025-07-28 13:08:54 +0900

    Delete discodeit directory
    delete past directory

--- discodeit/src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.mission.discodeit.service;
-
-import com.sprint.mission.discodeit.dto.AuthDto.LoginRequest;
-import com.sprint.mission.discodeit.dto.UserDto.UserResponseDto;
-
-public interface AuthService {
-
-    UserResponseDto login(LoginRequest requestDto);
-}
